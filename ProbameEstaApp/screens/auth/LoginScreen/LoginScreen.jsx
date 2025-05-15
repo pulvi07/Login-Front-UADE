43552bf3acd6bbd2945f4bb5e681e3735ef783cf
agus2k1 <agus2kcoin@outlook.com>
2025-05-15 00:06:45 -0300

    Feat: agrego funcionalidad para ver/ocultar contraseña

--- ProbameEstaApp/screens/auth/LoginScreen/LoginScreen.jsx
+++ ProbameEstaApp/screens/auth/LoginScreen/LoginScreen.jsx
@@ -8,11 +8,12 @@ import {
   TouchableOpacity,
   Alert,
 } from 'react-native';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const LoginScreen = ({setIsLoggedIn}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleLogin = () => {
     // BUG: comprueba cualquier contraseña q exista
@@ -21,7 +22,7 @@ const LoginScreen = ({setIsLoggedIn}) => {
     if (user) {
         setIsLoggedIn(true);
     } else {
-      Alert.alert('Error', 'Credenciales inválidas');
+      Alert.alert('Error', 'Email o contraseña incorrectos. Inténtalo de nuevo');
     }
   };
 
@@ -29,12 +30,28 @@ const LoginScreen = ({setIsLoggedIn}) => {
   return (
     <View style={styles.loginscreen}>
       <View style={{justifyContent: 'center', alignItems: 'center', gap: 20, width: '70%', height: '100%',}}>
-        <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 20 }}> Login </Text>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 20 }}> Inicia sesión </Text>
+        <Text style={styles.label}>Email</Text>
         <TextInput placeholder="Email" style={styles.input} keyboardType="email-address" autoCapitalize="none" value={email} onChangeText={setEmail}/>
-        <TextInput placeholder="Password" secureTextEntry style={styles.input} value={password} onChangeText={setPassword}/>
-        <TouchableOpacity style={{ backgroundColor: /[^a-zA-Z0-9]/.test(password) ? 'gray' : 'blue', padding: 10, borderRadius: 11,}} onPress={handleLogin}>
-
-          <Text style={{ fontSize: 18, fontWeight: '400', color: 'white' }}> Login</Text>
+        <Text style={styles.label}>Contraseña</Text>
+        <View style={styles.passwordContainer}>
+          <TextInput
+            placeholder="Ingresa tu contraseña"
+            secureTextEntry={!showPassword}
+            style={styles.passwordInput}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Ionicons
+              name={showPassword ? 'eye' : 'eye-off'}
+              size={24}
+              color="gray"
+            />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={{ backgroundColor: email.trim() === '' || password.trim() === '' ? 'gray' : 'blue', padding: 10, borderRadius: 11, marginTop: '5'}} disabled={email.trim() === '' || password.trim() === ''} onPress={handleLogin}>
+          <Text style={{ fontSize: 18, fontWeight: '400', color: 'white', paddingHorizontal: '20' }}>Login</Text>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => {}}>
@@ -58,6 +75,27 @@ export const styles = StyleSheet.create({
     width: '100%',
     borderWidth: 1,
     borderRadius: 11,
+    marginTop: -15
+  },
+  passwordContainer: {
+    width: '100%',
+    borderWidth: 1,
+    borderRadius: 11,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    marginTop: -15
+  },
+  passwordInput: {
+    flex: 1,
+    paddingVertical: 10,
+  },
+  label: {
+    fontSize: 14,
+    fontWeight: '500',
+    alignSelf: 'flex-start',
+    textAlign: 'left',
+    marginTop: '-10'
   },
 });
 
